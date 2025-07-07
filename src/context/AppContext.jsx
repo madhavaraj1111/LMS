750cab76deedffffdcf5b46cf872138755a7ed34
Madhavaraj C <madhavaraj1111@gmail.com>
2025-07-07 11:49:59 +0530

    Learnt Until the courses card section

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -3,11 +3,11 @@ import { createContext } from "react";
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
-    const value = {
- 
-    }
+  const currency = import.meta.env.VITE_CURRENCY;
+  const value = {
+    currency,
+  };
   return (
-  <AppContext.Provider value={value}>
-         {props.children}
-  </AppContext.Provider>);
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
 };
